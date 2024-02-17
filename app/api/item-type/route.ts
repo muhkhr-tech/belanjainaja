f05a093754cb016bcb6f6ce13dd5d9f9d97e6682
rafiq <rafiq.danova@gmail.com>
2024-02-17 19:35:48 +0800

    test revalidate api

--- app/api/item-type/route.ts
+++ app/api/item-type/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db"
 import { ItemType } from "@/lib/schema"
 
+export const revalidate = 60
+
 export async function GET(request: Request) {
 
     const resp = await db.select().from(ItemType)
